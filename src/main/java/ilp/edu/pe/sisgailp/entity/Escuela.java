1401c1f8650af9514df7c34141c51457c6f76764
eyza576 <eyza_10f@hotmail.com>
2022-04-26 17:55:22 -0500

    "se añadio las clase Escuela"

---
+++ src/main/java/ilp/edu/pe/sisgailp/entity/Escuela.java
@@ -0,0 +1,50 @@
+package ilp.edu.pe.sisgailp.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "escuela")
+public class Escuela {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idescuela;
+    private String denominacion;
+    private String codigoescuela;
+
+    public Escuela() {
+    }
+
+    public Escuela(Long idescuela) {
+        this.idescuela = idescuela;
+    }
+
+    public Escuela(Long idescuela, String denominacion, String codigoescuela) {
+        this.idescuela = idescuela;
+        this.denominacion = denominacion;
+        this.codigoescuela = codigoescuela;
+    }
+
+    public Long getIdescuela() {
+        return idescuela;
+    }
+
+    public void setIdescuela(Long idescuela) {
+        this.idescuela = idescuela;
+    }
+
+    public String getDenominacion() {
+        return denominacion;
+    }
+
+    public void setDenominacion(String denominacion) {
+        this.denominacion = denominacion;
+    }
+
+    public String getCodigoescuela() {
+        return codigoescuela;
+    }
+
+    public void setCodigoescuela(String codigoescuela) {
+        this.codigoescuela = codigoescuela;
+    }
+}
--- src/main/java/ilp/edu/pe/sisgailp/entity/Estudiante.java
+++ src/main/java/ilp/edu/pe/sisgailp/entity/Estudiante.java
@@ -1,9 +1,6 @@
 package ilp.edu.pe.sisgailp.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.PrimaryKeyJoinColumn;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
 
 
@@ -18,25 +15,41 @@ public class Estudiante extends  Persona{
     @Column(name = "serie",length = 10)
     private String serie;
 
-    public Estudiante() {
-    }
+@ManyToOne
+@JoinColumn(name = "idpersona")
+    private Escuela escuela;
 
-    public Estudiante(String codigo, String serie) {
+    public Estudiante(String codigo, String serie, Escuela escuela) {
         this.codigo = codigo;
         this.serie = serie;
+        this.escuela = escuela;
     }
 
-    public Estudiante(Long idpersona, String codigo, String serie) {
+    public Estudiante(Long idpersona, String codigo, String serie, Escuela escuela) {
         super(idpersona);
         this.codigo = codigo;
         this.serie = serie;
+        this.escuela = escuela;
     }
 
-    public Estudiante(String nombre, String apellido, int edad, String dni, Date fechanacimiento, String genero, String codigo, String serie) {
+    public Estudiante(String nombre, String apellido, int edad, String dni, Date fechanacimiento, String genero, String codigo, String serie, Escuela escuela) {
         super(nombre, apellido, edad, dni, fechanacimiento, genero);
         this.codigo = codigo;
         this.serie = serie;
+        this.escuela = escuela;
     }
+
+    public Escuela getEscuela() {
+        return escuela;
+    }
+
+    public void setEscuela(Escuela escuela) {
+        this.escuela = escuela;
+    }
+
+    public Estudiante() {
+    }
+
 
     public String getCodigo() {
         return codigo;
